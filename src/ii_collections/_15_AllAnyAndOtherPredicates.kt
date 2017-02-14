32e4fb122303d1df48392fef82e6c87f28f44da7
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2017-02-14 11:18:16 +0200

    koan 15

--- src/ii_collections/_15_AllAnyAndOtherPredicates.kt
+++ src/ii_collections/_15_AllAnyAndOtherPredicates.kt
@@ -13,27 +13,12 @@ fun example2(list: List<Int>) {
     val firstPositiveNumber: Int? = list.firstOrNull { it > 0 }
 }
 
-fun Customer.isFrom(city: City): Boolean {
-    // Return true if the customer is from the given city
-    todoCollectionTask()
-}
+fun Customer.isFrom(city: City): Boolean = city == this.city
 
-fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
-    // Return true if all customers are from the given city
-    todoCollectionTask()
-}
+fun Shop.checkAllCustomersAreFrom(city: City): Boolean = this.customers.all { city == it.city }
 
-fun Shop.hasCustomerFrom(city: City): Boolean {
-    // Return true if there is at least one customer from the given city
-    todoCollectionTask()
-}
+fun Shop.hasCustomerFrom(city: City): Boolean = this.customers.any { city == it.city }
 
-fun Shop.countCustomersFrom(city: City): Int {
-    // Return the number of customers from the given city
-    todoCollectionTask()
-}
+fun Shop.countCustomersFrom(city: City): Int = this.customers.count { city == it.city }
 
-fun Shop.findAnyCustomerFrom(city: City): Customer? {
-    // Return a customer who lives in the given city, or null if there is none
-    todoCollectionTask()
-}
+fun Shop.findAnyCustomerFrom(city: City): Customer? = this.customers.firstOrNull { city == it.city }
