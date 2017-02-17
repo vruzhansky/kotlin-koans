7d841500b17e4582554a53e1d98e192a293bbb4e
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2017-02-17 19:37:17 +0100

    koan 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if (this.year > other.year) 1
+        else if (this.year == other.year && this.month > other.month) 1
+        else if (this.year == other.year && this.month == other.month && this.dayOfMonth > other.dayOfMonth) 1
+        else if (this.year == other.year && this.month == other.month && this.dayOfMonth == other.dayOfMonth) 0
+        else -1
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
